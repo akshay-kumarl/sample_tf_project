7979a075f7801295f478b487729d130e756e87e2
Akshay L <110522215+akshay-kumarl@users.noreply.github.com>
2024-10-01 15:44:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,14 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+
 resource "aws_instance" "webserverexample" {
-  ami = "ami-0522ab6e1ddcc7055"
-  instance_type = "t2.micro"   
+  ami           = "ami-08718895af4dfa033"  # Use the latest Ubuntu AMI ID
+  instance_type = "t2.micro"              # Instance type
+  tags = {
+    Name = "Mumbai-EC2-Instance"  # Tag the instance
+    env = "dev"
+}
+  vpc_security_group_ids = ["sg-05cd9bab43a70250e"]
+  key_name = "keypairbylearnjan"  
 }
