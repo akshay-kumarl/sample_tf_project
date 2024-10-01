08bb0aeb64f0f9597291d929d65d47a7acd90953
Akshay L <110522215+akshay-kumarl@users.noreply.github.com>
2024-10-01 15:30:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAZQ22JFVNNW573YQH"
-  secret_key = "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"
 }
 
 resource "aws_instance" "webserverexample" {
